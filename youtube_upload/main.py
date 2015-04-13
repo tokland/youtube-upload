e2d94cfb123aaf95e98e05f0ac4f1d5a02512636
Arnau Sanchez <pyarnau@gmail.com>
2015-04-13 09:16:58 +0200

    auth.py: minor refactor

--- youtube_upload/main.py
+++ youtube_upload/main.py
@@ -124,7 +124,7 @@ def run_main(parser, options, args, output=sys.stdout):
     credentials = options.credentials_file or default_credentials
     debug("Using client secrets: {0}".format(client_secrets))
     debug("Using credentials file: {0}".format(credentials))
-    get_code_callback = (youtube_upload.auth._get_code_from_browser 
+    get_code_callback = (youtube_upload.auth.get_code_from_browser 
         if options.auth_gui else None)
     youtube = youtube_upload.auth.get_resource(client_secrets, credentials,
         get_code_callback=get_code_callback)
