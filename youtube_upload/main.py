21975a96125b662036f14b40481a13cf9211ad91
Craig Quiter <cquiter@gmail.com>
2019-05-25 15:02:36 -0700

    Support getting options for calling outside of command line

--- youtube_upload/main.py
+++ youtube_upload/main.py
@@ -193,67 +193,9 @@ def run_main(parser, options, args, output=sys.stdout):
     else:
         raise AuthenticationError("Cannot get youtube resource")
 
+
 def main(arguments):
-    """Upload videos to Youtube."""
-    usage = """Usage: %prog [OPTIONS] VIDEO [VIDEO2 ...]
-
-    Upload videos to Youtube."""
-    parser = optparse.OptionParser(usage)
-
-    # Video metadata
-    parser.add_option('-t', '--title', dest='title', type="string",
-        help='Video title')
-    parser.add_option('-c', '--category', dest='category', type="string",
-        help='Video category')
-    parser.add_option('-d', '--description', dest='description', type="string",
-        help='Video description')
-    parser.add_option('', '--description-file', dest='description_file', type="string",
-        help='Video description file', default=None)
-    parser.add_option('', '--tags', dest='tags', type="string",
-        help='Video tags (separated by commas: "tag1, tag2,...")')
-    parser.add_option('', '--privacy', dest='privacy', metavar="STRING",
-        default="public", help='Privacy status (public | unlisted | private)')
-    parser.add_option('', '--publish-at', dest='publish_at', metavar="datetime",
-       default=None, help='Publish date (ISO 8601): YYYY-MM-DDThh:mm:ss.sZ')
-    parser.add_option('', '--license', dest='license', metavar="string",
-       choices=('youtube', 'creativeCommon'), default='youtube',
-       help='License for the video, either "youtube" (the default) or "creativeCommon"')
-    parser.add_option('', '--location', dest='location', type="string",
-        default=None, metavar="latitude=VAL,longitude=VAL[,altitude=VAL]",
-        help='Video location"')
-    parser.add_option('', '--recording-date', dest='recording_date', metavar="datetime",
-        default=None, help="Recording date (ISO 8601): YYYY-MM-DDThh:mm:ss.sZ")
-    parser.add_option('', '--default-language', dest='default_language', type="string",
-        default=None, metavar="string",
-        help="Default language (ISO 639-1: en | fr | de | ...)")
-    parser.add_option('', '--default-audio-language', dest='default_audio_language', type="string",
-        default=None, metavar="string",
-        help="Default audio language (ISO 639-1: en | fr | de | ...)")
-    parser.add_option('', '--thumbnail', dest='thumb', type="string", metavar="FILE",
-        help='Image file to use as video thumbnail (JPEG or PNG)')
-    parser.add_option('', '--playlist', dest='playlist', type="string",
-        help='Playlist title (if it does not exist, it will be created)')
-    parser.add_option('', '--title-template', dest='title_template',
-        type="string", default="{title} [{n}/{total}]", metavar="string",
-        help='Template for multiple videos (default: {title} [{n}/{total}])')
-    parser.add_option('', '--embeddable', dest='embeddable', default=True,
-        help='Video is embeddable')
-
-    # Authentication
-    parser.add_option('', '--client-secrets', dest='client_secrets',
-        type="string", help='Client secrets JSON file')
-    parser.add_option('', '--credentials-file', dest='credentials_file',
-        type="string", help='Credentials JSON file')
-    parser.add_option('', '--auth-browser', dest='auth_browser', action='store_true',
-        help='Open a GUI browser to authenticate if required')
-
-    #Additional options
-    parser.add_option('', '--chunksize', dest='chunksize', type="int",
-        default = 1024*1024*8, help='Update file chunksize')
-    parser.add_option('', '--open-link', dest='open_link', action='store_true',
-        help='Opens a url in a web browser to display the uploaded video')
-
-    options, args = parser.parse_args(arguments)
+    args, options, parser = get_options(arguments)
 
     if options.description_file is not None and os.path.exists(options.description_file):
         with open(options.description_file, encoding="utf-8") as file:
@@ -264,6 +206,78 @@ def main(arguments):
     except googleapiclient.errors.HttpError as error:
         response = bytes.decode(error.content, encoding=lib.get_encoding()).strip()
         raise RequestError(u"Server response: {0}".format(response))
+
+
+def get_options(arguments):
+    """Upload videos to Youtube."""
+    usage = """Usage: %prog [OPTIONS] VIDEO [VIDEO2 ...]
+
+    Upload videos to Youtube."""
+    parser = optparse.OptionParser(usage)
+    # Video metadata
+    parser.add_option('-t', '--title', dest='title', type="string",
+                      help='Video title')
+    parser.add_option('-c', '--category', dest='category', type="string",
+                      help='Video category')
+    parser.add_option('-d', '--description', dest='description', type="string",
+                      help='Video description')
+    parser.add_option('', '--description-file', dest='description_file',
+                      type="string",
+                      help='Video description file', default=None)
+    parser.add_option('', '--tags', dest='tags', type="string",
+                      help='Video tags (separated by commas: "tag1, tag2,...")')
+    parser.add_option('', '--privacy', dest='privacy', metavar="STRING",
+                      default="public",
+                      help='Privacy status (public | unlisted | private)')
+    parser.add_option('', '--publish-at', dest='publish_at', metavar="datetime",
+                      default=None,
+                      help='Publish date (ISO 8601): YYYY-MM-DDThh:mm:ss.sZ')
+    parser.add_option('', '--license', dest='license', metavar="string",
+                      choices=('youtube', 'creativeCommon'), default='youtube',
+                      help='License for the video, either "youtube" (the default) or "creativeCommon"')
+    parser.add_option('', '--location', dest='location', type="string",
+                      default=None,
+                      metavar="latitude=VAL,longitude=VAL[,altitude=VAL]",
+                      help='Video location"')
+    parser.add_option('', '--recording-date', dest='recording_date',
+                      metavar="datetime",
+                      default=None,
+                      help="Recording date (ISO 8601): YYYY-MM-DDThh:mm:ss.sZ")
+    parser.add_option('', '--default-language', dest='default_language',
+                      type="string",
+                      default=None, metavar="string",
+                      help="Default language (ISO 639-1: en | fr | de | ...)")
+    parser.add_option('', '--default-audio-language',
+                      dest='default_audio_language', type="string",
+                      default=None, metavar="string",
+                      help="Default audio language (ISO 639-1: en | fr | de | ...)")
+    parser.add_option('', '--thumbnail', dest='thumb', type="string",
+                      metavar="FILE",
+                      help='Image file to use as video thumbnail (JPEG or PNG)')
+    parser.add_option('', '--playlist', dest='playlist', type="string",
+                      help='Playlist title (if it does not exist, it will be created)')
+    parser.add_option('', '--title-template', dest='title_template',
+                      type="string", default="{title} [{n}/{total}]",
+                      metavar="string",
+                      help='Template for multiple videos (default: {title} [{n}/{total}])')
+    parser.add_option('', '--embeddable', dest='embeddable', default=True,
+                      help='Video is embeddable')
+    # Authentication
+    parser.add_option('', '--client-secrets', dest='client_secrets',
+                      type="string", help='Client secrets JSON file')
+    parser.add_option('', '--credentials-file', dest='credentials_file',
+                      type="string", help='Credentials JSON file')
+    parser.add_option('', '--auth-browser', dest='auth_browser',
+                      action='store_true',
+                      help='Open a GUI browser to authenticate if required')
+    # Additional options
+    parser.add_option('', '--chunksize', dest='chunksize', type="int",
+                      default=1024 * 1024 * 8, help='Update file chunksize')
+    parser.add_option('', '--open-link', dest='open_link', action='store_true',
+                      help='Opens a url in a web browser to display the uploaded video')
+    options, args = parser.parse_args(arguments)
+    return args, options, parser
+
 
 def run():
     sys.exit(lib.catch_exceptions(EXIT_CODES, main, sys.argv[1:]))
